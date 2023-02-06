2f9268faa3c2b9e62c5a5441f38c8176afd6b68a
matinking82 <Matinking82@gmail.com>
2023-02-06 14:21:38 +0330

    Add project files.

---
+++ TestConsole/Program.cs
@@ -0,0 +1,405 @@
+﻿using UnitSelection.Core;
+
+namespace TestConsole
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Subject> subjects = new List<Subject>();
+            subjects.Add(new Subject()
+            {
+                SubjectName = "Pishrafteh",
+                Units = new List<Unit>()
+                {
+                    new Unit()
+                    {
+                        ProfName = "Maghsoodi (۱۲۲۱۰۰۳_۰۱)",
+                        DayOfUnit = new List<UnitDay>()
+                        {
+                            new UnitDay()
+                            {
+                                Day = 1,
+                                Hour = 3
+                            },
+                            new UnitDay()
+                            {
+                                Day = 3,
+                                Hour = 3
+                            }
+                        }
+                    }
+                }
+            });
+            subjects.Add(new Subject()
+            {
+                SubjectName = "Kargah",
+                Units = new List<Unit>()
+                {
+                    new Unit()
+                    {
+                        ProfName = "Chitgar (۱۲۲۱۱۲۸_۰۱)",
+                        DayOfUnit = new List<UnitDay>()
+                        {
+                            new UnitDay()
+                            {
+                                Day = 0,
+                                Hour = 0
+                            }
+                        }
+                    },
+                    new Unit()
+                    {
+                        ProfName = "Chitgar (۱۲۲۱۱۲۸_۰2)",
+                        DayOfUnit = new List<UnitDay>()
+                        {
+                            new UnitDay()
+                            {
+                                Day = 0,
+                                Hour = 1
+                            }
+                        }
+                    },
+                    new Unit()
+                    {
+                        ProfName = "Chitgar (۱۲۲۱۱۲۸_۰3)",
+                        DayOfUnit = new List<UnitDay>()
+                        {
+                            new UnitDay()
+                            {
+                                Day = 1,
+                                Hour = 0
+                            }
+                        }
+                    },
+                    new Unit()
+                    {
+                        ProfName = "Chitgar ۱۲۲۱۱۲۸_۰4",
+                        DayOfUnit = new List<UnitDay>()
+                        {
+                            new UnitDay()
+                            {
+                                Day = 1,
+                                Hour = 1
+                            }
+                        }
+                    },
+                }
+            });
+            subjects.Add(new Subject()
+            {
+                SubjectName = "Gosaste",
+                Units = new List<Unit>()
+                {
+                    new Unit()
+                    {
+                        ProfName = "Maghsoodi ۱۲۲۱۰۴۷_۰۱",
+                        DayOfUnit = new List<UnitDay>()
+                        {
+                            new UnitDay()
+                            {
+                                Day = 1,
+                                Hour = 1
+                            },
+                            new UnitDay()
+                            {
+                                Day = 3,
+                                Hour = 1
+                            }
+                        }
+                    },
+                    new Unit()
+                    {
+                        ProfName = "Kazemi ۱۲۲۱۰۴۷_۰۲",
+                        DayOfUnit = new List<UnitDay>()
+                        {
+                            new UnitDay()
+                            {
+                                Day = 1,
+                                Hour = 2
+                            },
+                            new UnitDay()
+                            {
+                                Day = 3,
+                                Hour = 2
+                            }
+                        }
+                    }
+                }
+            });
+            subjects.Add(new Subject()
+            {
+                SubjectName = "Math 2",
+                Units = new List<Unit>()
+                {
+                    new Unit()
+                    {
+                        ProfName = "Motiei ۱۱۱۱۰۰۲_۰۱",
+                        DayOfUnit = new List<UnitDay>()
+                        {
+                            new UnitDay()
+                            {
+                                Day = 0,
+                                Hour = 1
+                            },
+                            new UnitDay()
+                            {
+                                Day = 2,
+                                Hour = 3
+                            },
+                        }
+                    },
+                    new Unit()
+                    {
+                        ProfName = "Khademloo ۱۱۱۱۰۰۲_۰۲",
+                        DayOfUnit = new List<UnitDay>()
+                        {
+                            new UnitDay()
+                            {
+                                Day = 1,
+                                Hour = 2,
+                            },
+                            new UnitDay()
+                            {
+                                Day = 3,
+                                Hour = 0
+                            }
+                        }
+                    },
+                    new Unit()
+                    {
+                        ProfName = "Bakhshandeh ۱۱۱۱۰۰۲_۰۳",
+                        DayOfUnit = new List<UnitDay>()
+                        {
+                            new UnitDay()
+                            {
+                                Day = 1,
+                                Hour = 2
+                            },
+                            new UnitDay()
+                            {
+                                Day = 3,
+                                Hour = 1
+                            }
+                        }
+                    },
+                    new Unit()
+                    {
+                        ProfName = "Talaei ۱۱۱۱۰۰۲_۰۸",
+                        DayOfUnit = new List<UnitDay>()
+                        {
+                            new UnitDay()
+                            {
+                                Day = 0,
+                                Hour = 0
+                            },
+                            new UnitDay()
+                            {
+                                Day = 2,
+                                Hour = 0
+                            }
+                        }
+                    },
+                    new Unit()
+                    {
+                        ProfName = "Yadollah ۱۱۱۱۰۰۲_۱۰",
+                        DayOfUnit = new List<UnitDay>()
+                        {
+                            new UnitDay()
+                            {
+                                Day = 0,
+                                Hour = 2
+                            },
+                            new UnitDay()
+                            {
+                                Day = 2,
+                                Hour = 3
+                            }
+                        }
+                    },
+                }
+            });
+            subjects.Add(new Subject()
+            {
+                SubjectName = "Physics 2",
+                Units = new List<Unit>()
+                {
+                    new Unit()
+                    {
+                        ProfName = "Ghoreishi ۱۱۱۱۰۰۴_۰۷",
+                        DayOfUnit = new List<UnitDay>()
+                        {
+                            new UnitDay()
+                            {
+                                Day = 0,
+                                Hour = 2
+                            },
+                            new UnitDay()
+                            {
+                                Day = 3,
+                                Hour = 2
+                            }
+                        }
+                    },
+                    new Unit()
+                    {
+                        ProfName = "Ghoreishi ۱۱۱۱۰۰۴_۰8",
+                        DayOfUnit = new List<UnitDay>()
+                        {
+                            new UnitDay()
+                            {
+                                Day = 0,
+                                Hour = 3
+                            },
+                            new UnitDay()
+                            {
+                                Day = 3,
+                                Hour = 3
+                            }
+                        }
+                    },
+                    //new Unit()
+                    //{
+                    //    ProfName = "mehri ۱۱۱۱۰۰۴_۰۳",
+                    //    DayOfUnit = new List<UnitDay>()
+                    //    {
+                    //        new UnitDay()
+                    //        {
+                    //            Day = 2,
+                    //            Hour = 2
+                    //        },
+                    //        new UnitDay()
+                    //        {
+                    //            Day = 4,
+                    //            Hour = 1
+                    //        }
+                    //    }
+                    //},
+                }
+            });
+            subjects.Add(new Subject()
+            {
+                SubjectName = "Diff",
+                Units = new List<Unit>()
+                {
+                    new Unit()
+                    {
+                        ProfName = "Kalantary ۱۱۱۱۰۰۹_۰۹",
+                        DayOfUnit = new List<UnitDay>()
+                        {
+                            new UnitDay()
+                            {
+                                Day = 2,
+                                Hour = 0
+                            },
+                            new UnitDay()
+                            {
+                                Day = 4,
+                                Hour = 1
+                            }
+                        }
+                    },new Unit()
+                    {
+                        ProfName = "Motiei ۱۱۱۱۰۰۹_۰۱",
+                        DayOfUnit = new List<UnitDay>()
+                        {
+                            new UnitDay()
+                            {
+                                Day = 0,
+                                Hour = 0
+                            },
+                            new UnitDay()
+                            {
+                                Day = 2,
+                                Hour = 0
+                            }
+                        }
+                    },new Unit()
+                    {
+                        ProfName = "Motiei ۱۱۱۱۰۰۹_۰2",
+                        DayOfUnit = new List<UnitDay>()
+                        {
+                            new UnitDay()
+                            {
+                                Day = 1,
+                                Hour = 0
+                            },
+                            new UnitDay()
+                            {
+                                Day = 3,
+                                Hour = 0
+                            }
+                        }
+                    },
+                }
+            });
+            subjects.Add(new Subject()
+            {
+                SubjectName = "Andishe",
+                Units = new List<Unit>()
+                {
+                    new Unit()
+                    {
+                        ProfName = "heidary ۱۶۱۱۰۰۱_۰۲",
+                        DayOfUnit =new List<UnitDay>()
+                        {
+                            new UnitDay()
+                            {
+                                Day = 0,
+                                Hour = 3
+                            }
+                        }
+                    },
+                    //new Unit()
+                    //{
+                    //    ProfName = "heidary ۱۶۱۱۰۰۱_۰3",
+                    //    DayOfUnit =new List<UnitDay>()
+                    //    {
+                    //        new UnitDay()
+                    //        {
+                    //            Day =4,
+                    //            Hour = 3
+                    //        }
+                    //    }
+                    //},new Unit()
+                    //{
+                    //    ProfName = "heidary ۱۶۱۱۰۰۱_۰5",
+                    //    DayOfUnit =new List<UnitDay>()
+                    //    {
+                    //        new UnitDay()
+                    //        {
+                    //            Day = 4,
+                    //            Hour = 0,
+                    //        }
+                    //    }
+                    //},
+                }
+            });
+
+
+            RequestUnitSelectDto request = new RequestUnitSelectDto();
+            request.Subjects = subjects;
+            var result = UnitSelectionServices.SelectUnits(request);
+            StreamWriter writer = new StreamWriter("UnitSelectionResult.txt");
+            foreach (var UnitGroup in result.UnitGroups)
+            {
+                foreach (var unit in UnitGroup)
+                {
+                    Console.WriteLine($"{unit.SubjectName} : {unit.ProfName}");
+                    writer.WriteLine($"{unit.SubjectName} : {unit.ProfName}");
+                    foreach (var unitDay in unit.DayOfUnit)
+                    {
+                        Console.Write($" ({unitDay.Day}-{unitDay.Hour})");
+                        writer.Write($" ({unitDay.Day}-{unitDay.Hour})");
+                    }
+                    Console.Write("\n\n");
+                    writer.Write("\n\n");
+                }
+
+                Console.WriteLine("\n-------------------------------------------\n");
+                writer.WriteLine("\n-------------------------------------------\n");
+            }
+            writer.Close();
+        }
+    }
+}
---
+++ UnitSelection.Core/UnitSelectionServices.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitSelection.Core
+{
+    public static class UnitSelectionServices
+    {
+
+        public static async Task<UnitSelectionResultDto> SelectUnitsAsync(RequestUnitSelectDto request)
+        {
+            return await Task.Run(() =>
+            {
+                return SelectUnits(request);
+            });
+        }
+
+        public static UnitSelectionResultDto SelectUnits(RequestUnitSelectDto request)
+        {
+
+            UnitSelectionResultDto result = new UnitSelectionResultDto();
+
+            int subjectsCount = request.Subjects.Count();
+
+            int[] indexes = new int[subjectsCount];
+
+            int firstCount = request.Subjects.ElementAt(0).Units.Count();
+
+            while (indexes[0] < firstCount)
+            {
+                List<Unit> units = new List<Unit>();
+                for (int i = 0; i < subjectsCount; i++)
+                {
+                    var unit = request.Subjects.ElementAt(i).Units.ElementAt(indexes[i]);
+                    unit.SubjectName = request.Subjects.ElementAt(i).SubjectName;
+                    units.Add(unit);
+                }
+
+                if (IsUnitsOk(units))
+                {
+                    result.UnitGroups.Add(units);
+                }
+
+                bool IsInc = true;
+                for (int i = subjectsCount - 1; i >= 0 && IsInc; i--)
+                {
+                    indexes[i]++;
+                    IsInc = false;
+                    if (indexes[i] >= request.Subjects.ElementAt(i).Units.Count())
+                    {
+                        if (i == 0)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            indexes[i] = 0;
+                            IsInc = true;
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private static bool IsUnitsOk(IEnumerable<Unit> units)
+        {
+            bool[,] Table = new bool[5, 5];
+
+            foreach (var unit in units)
+            {
+                foreach (var unitDay in unit.DayOfUnit)
+                {
+                    if (Table[unitDay.Day, unitDay.Hour])
+                    {
+                        return false;
+                    }
+
+                    Table[unitDay.Day, unitDay.Hour] = true;
+                }
+            }
+
+            return true;
+        }
+    }
+
+    public record RequestUnitSelectDto
+    {
+        public IEnumerable<Subject> Subjects { get; set; }
+    }
+
+    public record UnitSelectionResultDto
+    {
+        public List<IEnumerable<Unit>> UnitGroups { get; set; } = new List<IEnumerable<Unit>>();
+    }
+
+    public record Subject
+    {
+        public string SubjectName { get; set; }
+        public IEnumerable<Unit> Units { get; set; }
+    }
+
+    public record Unit
+    {
+        public string ProfName { get; set; }
+        public string SubjectName { get; set; }
+        public IEnumerable<UnitDay> DayOfUnit { get; set; }
+    }
+
+    public record UnitDay
+    {
+        public int Day { get; set; }
+        /*
+         * 0: Saturday
+         * 1: Sunday
+         * 2: Monday
+         * 3: Tuesday
+         * 4: Wednesday
+         */
+        public int Hour { get; set; }
+        /*
+         * 0: 8
+         * 1: 10
+         * 2: 13
+         * 3: 15
+         * 4: 17
+         */
+    }
+}
