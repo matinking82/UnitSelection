c7892d6539163cadb9d9879fad075b74d861d79d
matinking82 <Matinking82@gmail.com>
2023-09-07 14:54:23 +0330

    add save and load

--- UnitSelection.Blazor/Client/Program.cs
+++ UnitSelection.Blazor/Client/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using UnitSelection.Blazor;
@@ -11,7 +12,7 @@ namespace UnitSelection.Blazor
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
-
+            builder.Services.AddBlazoredLocalStorage();
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
---
+++ UnitSelection.Blazor/Types/SaveItem.cs
@@ -0,0 +1,11 @@
+﻿using UnitSelection.Core;
+
+namespace UnitSelection.Blazor.Types
+{
+    public record SaveItem
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public List<Subject> Subjects { get; set; }
+    }
+}
